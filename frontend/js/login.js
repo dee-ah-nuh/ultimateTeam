a5acd921d6a878402f6b217b54ae419b8861603b
Diana Valladares <dvalladares@aarete.com>
2024-03-23 21:27:42 -0500

    updated login

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,29 +1,9 @@
 document.getElementById('loginForm').addEventListener('submit', function(e) {
-    e.preventDefault();
+    e.preventDefault(); // Prevent the default form submission
 
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    // Simulate a login process
+    console.log('Logging in...');
 
-    fetch('http://localhost:8000/api/login/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            // Include CSRF token if needed
-        },
-        body: JSON.stringify({ email: email, password: password })
-    })
-    .then(response => {
-        if (response.ok) {
-            return response.json(); // Process the success response
-        }
-        throw new Error('Login failed'); // Handle login failure
-    })
-    .then(data => {
-        console.log(data.message);
-        window.location.href = 'choose-sport.html'; // Redirect on success
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        // Optionally, inform the user that login failed
-    });
+    // Redirect to the profile page as an example
+    window.location.href = 'profile.html';
 });
